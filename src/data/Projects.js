c28390ef24e35d6b09bc987bff28a4bb8d68c833
zeez09 <harunaabdulazeez766@gmail.com>
2025-05-29 08:56:45 +0100

    fixed images

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,35 +1,45 @@
+import image1 from "../assets/images/Screenshot (160).png";
+import image2 from "../assets/images/Screenshot (133).png";
+import image3 from "../assets/images/Screenshot (139).png";
+import image4 from "../assets/images/Screenshot (158).png";
+import image5 from "../assets/images/Screenshot (159).png";
+
+
+
+
+
 const projects = [
     {
         id: "1",
-        image:"src/assets/images/Screenshot (132).png",
-        title: "typefrenzy",
-        description: "A simple mobile responsive landing page.",
-        link: "https://typefrenzy.netlify.app/",
+        image: image1,
+        title: "sneakeefy",
+        description: "A simple e-commerce web app",
+        link: "https://sneakeefy.netlify.app/",
     },
     {
         id: "2",
-        image:"src/assets/images/Screenshot (133).png",
+        image: image2,
         title: "dessert-court",
         description: "A food odering web app",
         link: "https://dessert-court.netlify.app/",
     },
     {
         id: "3",
-        image:"src/assets/images/Screenshot (139).png",
+        image: image3,
         title: "MQ-flight-booking-app",
         description: "A flight odering web app",
         link: "https://mq-flight-booking-app.vercel.app/",
     },
     {
         id: "4",
-        image:"src/assets/images/Screenshot (158).png",
+        image: image4,
         title: "loop-studios",
         description: "A simple mobile responsive landing page",
         link: "https://loop-studios-plum-kappa.vercel.app/",
     },
     {
         id: "5",
-        image:"src/assets/images/Screenshot (159).png",
+        image: image5,
         title: "zedtunes",
         description: "A music playing web app",
         link: "https://zedtunes.netlify.app/",

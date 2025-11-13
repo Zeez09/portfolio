760a4a7913422dc576f6990798000a8800982bfa
zeez09 <harunaabdulazeez766@gmail.com>
2025-11-13 23:59:36 +0100

    updated navigation

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,10 +1,4 @@
-import image1 from "../assets/images/Screenshot (160).png";
-import image2 from "../assets/images/Screenshot (133).png";
-import image3 from "../assets/images/Screenshot (139).png";
-import image4 from "../assets/images/Screenshot (158).png";
-import image5 from "../assets/images/Screenshot (159).png";
-import image6 from "../assets/images/Screenshot (207).png";
-import image7 from "../assets/images/Screenshot (208).png";
+
 
 
 
@@ -13,35 +7,35 @@ import image7 from "../assets/images/Screenshot (208).png";
 const projects = [
     {
         id: "1",
-        image: image1,
+        image:"/images/Screenshot (160).png", 
         title: "sneakeefy",
         description: "A simple e-commerce web app",
         link: "https://sneakeefy.netlify.app/",
     },
     {
         id: "2",
-        image: image2,
+        image: "/images/Screenshot (133).png",
         title: "dessert-court",
         description: "A food odering web app",
         link: "https://dessert-court.netlify.app/",
     },
     {
         id: "3",
-        image: image3,
+        image: "/images/Screenshot (139).png",
         title: "MQ-flight-booking-app",
         description: "A flight booking web app",
         link: "https://mq-flight-booking-app.vercel.app/",
     },
     {
         id: "4",
-        image: image4,
+        image: "/images/Screenshot (158).png",
         title: "loop-studios",
         description: "A simple mobile responsive landing page",
         link: "https://loop-studios-plum-kappa.vercel.app/",
     },
     {
         id: "5",
-        image: image5,
+        image: "/images/Screenshot (159).png",
         title: "zedtunes",
         description: "A music playing web app",
         link: "https://zedtunes.netlify.app/",
@@ -49,14 +43,14 @@ const projects = [
 
     {
         id: "6",
-        image: image6,
+        image: "/images/Screenshot (207).png",
         title: "finsweet",
         description: "A simple static multipage website",
         link: "https://finsweet-navy.vercel.app/",
     },
     {
         id: "7",
-        image: image7,
+        image: "/images/Screenshot (208).png",
         title: "Delish",
         description: "A modern food ordering and blog website.",
         link: "https://delish-five.vercel.app/"

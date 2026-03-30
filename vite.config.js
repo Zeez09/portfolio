b4ec7f8ec7c8faca3a954c0e8de0ee8f0c1f2ea7
zeez09 <harunaabdulazeez766@gmail.com>
2026-03-30 18:51:24 +0100

    new updates

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || "/", // <- automatically pick base path
   plugins: [react(), tailwindcss()],
-  build: {
-    chunkSizeWarningLimit: 800,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "react-vendor";
-            if (id.includes("gsap")) return "gsap";
-            if (id.includes("swiper")) return "swiper";
-            if (id.includes("react-icons") || id.includes("lucide-react")) return "icons";
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
-});
+})

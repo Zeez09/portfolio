b8c1d9fc92937f5d44eadf96cf384a6d676d5714
zeez09 <harunaabdulazeez766@gmail.com>
2026-03-30 14:17:17 +0100

    new updates ft optimisation

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,37 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  build: {
+    chunkSizeWarningLimit: 800, // optional (hides warning)
+
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+
+            if (id.includes("react")) {
+              return "react";
+            }
+
+            if (id.includes("gsap")) {
+              return "gsap";
+            }
+
+            if (id.includes("swiper")) {
+              return "swiper";
+            }
+
+            if (id.includes("emailjs")) {
+              return "email";
+            }
+
+            return "vendor";
+          }
+        }
+      }
+    }
+  }
 })
